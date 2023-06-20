d3cb628d3502b21d038e40be89b0cb5b61c60241
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2023-06-20 13:10:36 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import Navbar from '@/components/Navbar'
+import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/Toaster'
+import { cn } from '@/lib/utils'
+import '@/styles/globals.css'
+import {Inter} from 'next/font/google'
+
+export const metadata = {
+  title: 'Breadit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+
+const inter = Inter({subsets: ['latin']})
+
+export default function RootLayout({
+  children,
+  authModal
+}: {
+  children: React.ReactNode
+  authModal: React.ReactNode
+}) {
+  return (
+    <html 
+      lang='en' 
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className)
+      }>
+      <body className='min-h-screen bg-slate-50 antialiased'>
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+
+          {authModal}
+
+          <div className='container max-w-7xl max-auto h-full pt-12'>
+            {children}
+          </div>
+
+          <Toaster />
+        </Providers>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,31 @@
+import { Avatar, AvatarFallback } from './ui/Avatar'
+import { User } from 'next-auth'
+import Image from 'next/image'
+import { FC } from 'react'
+import { Icons } from './Icons'
+import { AvatarProps } from '@radix-ui/react-avatar'
+
+interface UserAvatarProps extends AvatarProps{
+    user: Pick<User, 'name' | 'image'>
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({user, ...props}) => {
+  return <Avatar {...props}>
+    {user.image ? (
+      <div className='relative aspect-square h-full w-full'>
+        <Image 
+          fill 
+          src={user.image} 
+          alt='profile-picture'
+          referrerPolicy='no-referrer'
+        />
+      </div>) : (
+      <AvatarFallback>
+        <span className='sr-only'>{user?.name}</span>
+        <Icons.user className='h-4 w-4' />
+      </AvatarFallback>
+    )}
+  </Avatar>
+}
+
+export default UserAvatar
