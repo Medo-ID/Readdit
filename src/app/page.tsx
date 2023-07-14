1a7e04a402bb0dcf61a84b2ec68145ef0ee4e9fb
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2023-07-14 12:17:52 +0100

    last update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { getAuthSession } from "@/lib/auth";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 export default async function Home() {
   const session = await getAuthSession()
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
         <div className="sticky top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
                 <Link href='/' className="flex gap-2 items-center">
-                    <Image src="/book.png" alt="app-logo" height={500} width={500} className="h-8 w-10 sm:h-6 sm:w-6" />
+                    <Image src="/favicon.png" alt="app-logo" height={500} width={500} className="h-8 w-10 sm:h-6 sm:w-6" />
                     <p className="hidden text-zinc-700 text-sm font-medium md:block">Readdit</p>
                 </Link>
                 
